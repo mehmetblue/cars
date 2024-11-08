945220ff53003a762c15a6c78092cdf5b6716273
Mehmet Koç <98655470+mehmetblue@users.noreply.github.com>
2024-11-08 09:44:18 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+mongodb_config = {
+    'username': 'infot2409',
+    'password': 'Ninx8NlovKXWD1nF',
+    'port': 27017,
+    'database_name': 'cars_db',
+    'collection_name': 'car_col',
+}
